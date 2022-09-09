ffd41f143f1e3aba089587573cd997491639e5c5
FabricioMMN <112163899+FabricioMMN@users.noreply.github.com>
2022-09-09 09:53:36 +0200

    hello again!

--- Hello.java
+++ Hello.java
@@ -1,7 +1,11 @@
 public class Hello
 {
-    public static void main (String [] args)
+    public static void main(String[] args)
     {
-        System.out.println("Hello World");
+       String helloWorld[] = {"H", "e", "l", "l", "o"};
+       for(int i = 0; i < helloWorld.length; i++) {
+        String str = helloWorld [i];
+        System.out.println(str);
+     }
     }
-}
+ }
